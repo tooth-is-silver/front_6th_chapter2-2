644d9701da9848a94bb4b252a27d4c3fc0161579
Ga eun Lee <35989487+tooth-is-silver@users.noreply.github.com>
2025-08-08 06:00:22 +0900

    remove

--- src/advanced/atoms/selectedCoupon.ts
+++
@@ -1,4 +0,0 @@
-import { atom } from 'jotai';
-import { Coupon } from '../../types';
-
-export const selectedCouponAtom = atom<Coupon | null>(null);
--- src/refactoring(hint)/main.tsx
+++
@@ -1,4 +0,0 @@
-// TODO: React 앱 엔트리 포인트
-// App 컴포넌트를 root DOM 요소에 렌더링
-
-// TODO: 구현
